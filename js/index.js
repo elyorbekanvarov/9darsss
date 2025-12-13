d235b070ac5826d5eed28dcfae91e5995b21b131
elyorbekanvarov <e0275king@gmail.com>
2025-12-13 17:20:17 +0300

    end

---
+++ js/index.js
@@ -0,0 +1,42 @@
+const SunEl = document.querySelector(".sun");
+const MoonEl = document.querySelector(".moon");
+const Body = document.body;
+const Ul = document.querySelector("ul");
+
+function ToggleMode() {
+  Body.classList.toggle("dark-mode");
+  MoonEl.classList.toggle("hidden");
+  SunEl.classList.toggle("hidden");
+}
+
+MoonEl.addEventListener("click", () => {
+  ToggleMode();
+});
+SunEl.addEventListener("click", () => {
+  ToggleMode();
+});
+
+function UpdateUi(arr) {
+  Ul.innerHTML = "";
+  arr.forEach((user, index) => {
+    let { name } = user;
+    Ul.innerHTML += `
+      <li data-index="${index}">
+        ${name}
+        <button class="delete-btn" style="margin-left: 10px; cursor: pointer;">X</button>
+      </li>
+    `;
+  });
+
+  const deleteButtons = document.querySelectorAll(".delete-btn");
+  deleteButtons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const li = e.target.parentElement;
+      li.remove();
+    });
+  });
+}
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((res) => res.json())
+  .then((data) => UpdateUi(data))
+  .catch((err) => console.log(err));
